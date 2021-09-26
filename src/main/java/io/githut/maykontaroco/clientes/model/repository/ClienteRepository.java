5ec62bc3a417c30101feea79d96095a8afd045b7
Maykon Taroco <maykontaroco@gmail.com>
2021-09-25 22:48:56 -0300

    Create repository Cliente and Servico

---
+++ src/main/java/io/githut/maykontaroco/clientes/model/repository/ClienteRepository.java
@@ -0,0 +1,6 @@
+package io.githut.maykontaroco.clientes.model.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ClienteRepository extends JpaRepository {
+}
