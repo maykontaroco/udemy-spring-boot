79e7bf73d269c3e5e23abc4247e6e27c01770233
Maykon Taroco <maykontaroco@gmail.com>
2021-09-27 22:54:24 -0300

    ClienteController and all resources.

--- src/main/java/io/github/maykontaroco/clientes/model/entity/Cliente.java
+++ src/main/java/io/github/maykontaroco/clientes/model/entity/Cliente.java
@@ -4,9 +4,12 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.br.CPF;
 
 import javax.persistence.*;
 import java.time.LocalDate;
+import javax.validation.constraints.*;
+
 
 @Entity
 @Data
@@ -20,9 +23,12 @@ public class Cliente {
     private Integer id;
 
     @Column(nullable = false, length = 150)
+    @NotEmpty(message = "{campo.nome.obrigatorio}")
     private String nome;
 
-    @Column(nullable = false, length = 11)
+    @Column(nullable = false, length = 11, updatable = false)
+    @NotNull(message = "{campo.cpf.obrigatorio}")
+    @CPF(message = "{campo.cpf.invalido}")
     private String cpf;
 
     @Column
