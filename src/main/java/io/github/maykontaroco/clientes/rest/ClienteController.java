188cf08b8031c27a05a6844760a4dfbc06abbcbc
Maykon Taroco <maykontaroco@gmail.com>
2021-09-27 22:54:43 -0300

    Add CORS

--- src/main/java/io/github/maykontaroco/clientes/rest/ClienteController.java
+++ src/main/java/io/github/maykontaroco/clientes/rest/ClienteController.java
@@ -13,6 +13,7 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/clientes")
+@CrossOrigin("http://localhost:4200")
 public class ClienteController {
 
     private final ClienteRepository repository;
